42940952958e29c4f55b83ec66a9a24c496329e8
Bruno Arthur <brunoartc@yahoo.com>
2019-04-08 17:10:55 -0300

    add IDLE blackscreen

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -59,7 +59,7 @@ void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 #define APERTADO '1'
 #define LIBERADO '0'
 
-typedef void (*p_handler) (uint32_t, uint32_t);
+typedef void(*p_handler) (uint32_t, uint32_t);
 
 typedef struct {
 	uint32_t PIO_NAME;
@@ -81,9 +81,9 @@ volatile Bool but_status;
 
 volatile Bool f_rtt_alarme = false;
 
-botao BUT1 = {.PIO_NAME = PIOA, .PIO_ID = ID_PIOA, .PIO_IDX = 11u, .PIO_MASK = (1u << 11u), .BUT_NUM = '1'}; //botao placa para reset
-botao BUT2 = {.PIO_NAME = PIOB, .PIO_ID = ID_PIOB, .PIO_IDX = 2u, .PIO_MASK = (1u << 2u), .BUT_NUM = '2'}; //sensor
-botao BUT3 = {.PIO_NAME = PIOA, .PIO_ID = ID_PIOA, .PIO_IDX = 3u, .PIO_MASK = (1u << 3u), .BUT_NUM = '3'};
+botao BUT1 = { .PIO_NAME = PIOA,.PIO_ID = ID_PIOA,.PIO_IDX = 11u,.PIO_MASK = (1u << 11u),.BUT_NUM = '1' }; //botao placa para reset
+botao BUT2 = { .PIO_NAME = PIOB,.PIO_ID = ID_PIOB,.PIO_IDX = 2u,.PIO_MASK = (1u << 2u),.BUT_NUM = '2' }; //sensor
+botao BUT3 = { .PIO_NAME = PIOA,.PIO_ID = ID_PIOA,.PIO_IDX = 3u,.PIO_MASK = (1u << 3u),.BUT_NUM = '3' };
 
 
 /************************************************************************/
@@ -113,7 +113,7 @@ void RTT_Handler(void)
 
 	/* IRQ due to Alarm */
 	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
-		
+
 		f_rtt_alarme = true;                  // flag RTT alarme
 	}
 }
@@ -122,11 +122,13 @@ void RTT_Handler(void)
 /* funcoes                                                              */
 /************************************************************************/
 
-void pin_toggle(Pio *pio, uint32_t mask) {
-	if (pio_get_output_data_status(pio, mask))
-	pio_clear(pio, mask);
-	else
-	pio_set(pio, mask);
+
+void draw_screen(void) {
+
+	ili9488_set_foreground_color(COLOR_CONVERT(COLOR_BLACK));
+
+	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH - 1, ILI9488_LCD_HEIGHT - 1);
+
 }
 
 
@@ -155,43 +157,43 @@ static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
 void but1_callback(void)
 {
 	BUT1.but_flag = true;
-	if(!pio_get(BUT1.PIO_NAME, PIO_INPUT, BUT1.PIO_MASK))
-	but_status = APERTADO;
+	if (!pio_get(BUT1.PIO_NAME, PIO_INPUT, BUT1.PIO_MASK))
+		but_status = APERTADO;
 	else
-	but_status = LIBERADO;
+		but_status = LIBERADO;
 }
 
 void but2_callback(void)
 {
 	BUT2.but_flag = true;
-	if(!pio_get(BUT2.PIO_NAME, PIO_INPUT, BUT2.PIO_MASK))
-	but_status = APERTADO;
+	if (!pio_get(BUT2.PIO_NAME, PIO_INPUT, BUT2.PIO_MASK))
+		but_status = APERTADO;
 	else
-	but_status = LIBERADO;
+		but_status = LIBERADO;
 }
 
 void but3_callback(void)
 {
 	BUT3.but_flag = true;
-	if(!pio_get(BUT3.PIO_NAME, PIO_INPUT, BUT3.PIO_MASK))
-	but_status = APERTADO;
+	if (!pio_get(BUT3.PIO_NAME, PIO_INPUT, BUT3.PIO_MASK))
+		but_status = APERTADO;
 	else
-	but_status = LIBERADO;
+		but_status = LIBERADO;
 }
 
-void iniciabots(botao BOT, p_handler *funcao){
-	
-	pio_configure(BOT.PIO_NAME, PIO_INPUT, BOT.PIO_MASK, PIO_PULLUP|PIO_DEBOUNCE);
+void iniciabots(botao BOT, p_handler *funcao) {
+
+	pio_configure(BOT.PIO_NAME, PIO_INPUT, BOT.PIO_MASK, PIO_PULLUP | PIO_DEBOUNCE);
 	pio_set_debounce_filter(BOT.PIO_NAME, BOT.PIO_MASK, 60);
 
 	// Configura interrupção no pino referente ao botao e associa
 	// função de callback caso uma interrupção for gerada
 	// a função de callback é a: but_callback()
 	pio_handler_set(BOT.PIO_NAME,
-	BOT.PIO_ID,
-	BOT.PIO_MASK,
-	PIO_IT_FALL_EDGE,
-	funcao);
+		BOT.PIO_ID,
+		BOT.PIO_MASK,
+		PIO_IT_FALL_EDGE,
+		funcao);
 
 	// Ativa interrupção
 	pio_enable_interrupt(BOT.PIO_NAME, BOT.PIO_MASK);
@@ -208,7 +210,7 @@ void iniciabots(botao BOT, p_handler *funcao){
 
 void io_init(void)
 {
-	
+
 	iniciabots(BUT1, but1_callback);
 	iniciabots(BUT2, but2_callback);
 	iniciabots(BUT3, but3_callback);
@@ -237,26 +239,26 @@ int main(void) {
 	long revolucoes = 0;
 	long revolucoes_insta = 0;
 	float vel_max = 0;
-	long idle=0;
+	long idle = 0;
 
 	f_rtt_alarme = true;
 
-	
+
 
 	while (1) {
-		if(BUT1.but_flag) {
+		if (BUT1.but_flag) {
 			revolucoes++;
 			revolucoes_insta++;
 			BUT1.but_flag = false;
-			idle=0;
+			idle = 0;
 		}
-		if(BUT2.but_flag) {
-			revolucoes=0;
-			tempo_total=0; //botao para reiniciar
+		if (BUT2.but_flag) {
+			revolucoes = 0;
+			tempo_total = 0; //botao para reiniciar
 			BUT2.but_flag = false;
 		}
-		
-		
+
+
 		if (f_rtt_alarme) {
 
 			/*
@@ -292,62 +294,60 @@ int main(void) {
 			/*
 			* CLEAR FLAG
 			*/
-			if (banana){
+			if (banana) {
 				font_draw_text(&sourcecodepro_28, "NAOMUNDO", 50, 50, 1);
-				banana=!banana;
-				} else {
+				banana = !banana;
+			}
+			else {
 				font_draw_text(&sourcecodepro_28, "SIMMUNDO", 50, 50, 1);
 				banana = !banana;
 			}
-			
-			if (idle<=20){
+
+			if (idle <= 20) {
 				char tempo[12];
-				sprintf(tempo, "%d:%d:%d  ",  tempo_total/3600, (tempo_total%3600)/60, tempo_total%3600);
-				
-				font_draw_text(&calibri_36, tempo, 10, 100-36, 1); // + 36 sempre -> \n
-				sprintf(tempo, "t_t:%d  ",  tempo_total);
-				tempo_total+=2;
-				idle+=2;
+				sprintf(tempo, "%d:%d:%d  ", tempo_total / 3600, (tempo_total % 3600) / 60, tempo_total % 3600);
+
+				font_draw_text(&calibri_36, time(tempo_total), 10, 100 - 36, 1); // + 36 sempre -> \n
+				sprintf(tempo, "t_t:%d  ", tempo_total);
+				tempo_total += 2;
+				idle += 2;
 				font_draw_text(&calibri_36, tempo, 10, 100, 1); // + 36 sempre -> \n
-				sprintf(tempo, "d_t:%f m ",  revolucoes*2*0.325*PI);
-				font_draw_text(&calibri_36, tempo, 10 , 100+ 36*1, 1); // + 36 sempre -> \n
-				sprintf(tempo, "v_me:%f km/h ",  ((revolucoes*2*0.325*PI)/tempo_total)*3.6);
-				font_draw_text(&calibri_36, tempo, 10 , 100+ 36*2, 1); // + 36 sempre -> \n
-				sprintf(tempo, "v_i:%f m/s ",  (revolucoes_insta*2*0.325*PI)/2);
-				font_draw_text(&calibri_36, tempo, 10 , 100+ 36*3, 1); // + 36 sempre -> \n
-				if ((revolucoes_insta*2*0.325*PI)/2 > vel_max){
-					vel_max = (revolucoes_insta*2*0.325*PI)/2;
-					font_draw_text(&calibri_36, "+vel acelerando      ", 10 , 100+ 36*4, 1); // + 36 sempre -> \n
-					} else {
-					font_draw_text(&calibri_36, "-vel desacelerando   ", 10 , 100+ 36*4, 1); // + 36 sempre -> \n !!ta errado
+				sprintf(tempo, "d_t:%f m ", revolucoes * 2 * 0.325*PI);
+				font_draw_text(&calibri_36, tempo, 10, 100 + 36 * 1, 1); // + 36 sempre -> \n
+				sprintf(tempo, "v_me:%f km/h ", ((revolucoes * 2 * 0.325*PI) / tempo_total)*3.6);
+				font_draw_text(&calibri_36, tempo, 10, 100 + 36 * 2, 1); // + 36 sempre -> \n
+				sprintf(tempo, "v_i:%f m/s ", (revolucoes_insta * 2 * 0.325*PI) / 2);
+				font_draw_text(&calibri_36, tempo, 10, 100 + 36 * 3, 1); // + 36 sempre -> \n
+				if ((revolucoes_insta * 2 * 0.325*PI) / 2 > vel_max) {
+					vel_max = (revolucoes_insta * 2 * 0.325*PI) / 2;
+					font_draw_text(&calibri_36, "+vel acelerando      ", 10, 100 + 36 * 4, 1); // + 36 sempre -> \n
 				}
-				sprintf(tempo, "v_max:%f m/s ",  vel_max);
-				font_draw_text(&calibri_36, tempo, 10 , 100+ 36*3, 1); // + 36 sempre -> \n
-				revolucoes_insta =0;
-				
-			} else {
+				else {
+					font_draw_text(&calibri_36, "-vel desacelerando   ", 10, 100 + 36 * 4, 1); // + 36 sempre -> \n !!ta errado
+				}
+				sprintf(tempo, "v_max:%f m/s ", vel_max);
+				font_draw_text(&calibri_36, tempo, 10, 100 + 36 * 3, 1); // + 36 sempre -> \n
+				revolucoes_insta = 0;
+
+			}
+			else {
 				//paro a contagem
 				//desligar LCD? nao sei
-				font_draw_text(&calibri_36, "IDLE                      ", 10 , 100- 36*1, 1); // + 36 sempre -> \n
-				font_draw_text(&calibri_36, "IDLE                      ", 10 , 100+ 36*0, 1); // + 36 sempre -> \n
-				font_draw_text(&calibri_36, "IDLE                      ", 10 , 100+ 36*1, 1); // + 36 sempre -> \n
-				font_draw_text(&calibri_36, "IDLE                      ", 10 , 100+ 36*2, 1); // + 36 sempre -> \n
-				font_draw_text(&calibri_36, "IDLE                      ", 10 , 100+ 36*3, 1); // + 36 sempre -> \n
-				font_draw_text(&calibri_36, "IDLE                      ", 10 , 100+ 36*4, 1); // + 36 sempre -> \n
-				
+				draw_screen();
+
 			}
-			
-			
-			
-			
-			
-			
+
+
+
+
+
+
 
 			f_rtt_alarme = false;
 		}
 		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
-		
-		
+
+
 
 
 	}
